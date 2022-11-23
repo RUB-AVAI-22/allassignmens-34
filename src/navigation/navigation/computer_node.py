f45082bad7594809d34cb1e567bc7ba114cfca47
Fabian Kievelitz <fabian.kievelitz@ruhr-uni-bochum.de>
2022-11-23 16:11:22 +0100

    removed gamepad support and fixed acceleration bugs

--- src/navigation/navigation/computer_node.py
+++ src/navigation/navigation/computer_node.py
@@ -11,9 +11,6 @@ import cv2
 import numpy as np
 from pynput import keyboard
 
-from inputs import get_gamepad
-from inputs import UnpluggedError
-
 
 class Computer_Node(Node):
 
@@ -23,7 +20,6 @@ class Computer_Node(Node):
         self.maxTransVelocity = 0.26 #in m/s
         self.maxRotVelocity = 1.82 #in rad/s
         self.speed = 0.1
-        self.acceleration = 0.01
         
         self.currentMovement = Vector3() #x = translational, z = rotational
         self.desiredMovement = Vector3() #x = translational, z = rotational
@@ -35,10 +31,9 @@ class Computer_Node(Node):
         self.bridge = CvBridge()
 
         self.updateFrequency = 20
+        self.acceleration = 0.1/self.updateFrequency
         self.velocityUpdateTimer = self.create_timer(1 / self.updateFrequency, self.updateVelocity)
-        
-        self.controller = True
-        self.controllerInputTimer = self.create_timer(1/self.updateFrequency, self.updateControllerInput)
+
         
         self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         self.listener.start()
@@ -55,28 +50,35 @@ class Computer_Node(Node):
         if key == keyboard.Key.up or key == keyboard.Key.down or keyboard.Key.left or keyboard.Key.right or keyboard.Key.shift or keyboard.Key.ctrl or keyboard.Key.space:
 
             if key == keyboard.Key.up:
-                self.desiredMovement.x = self.speed
+                self.desiredMovement.x = self.speed*self.maxTransVelocity
 
             elif key == keyboard.Key.down:
-                self.desiredMovement.x = -self.speed
+                self.desiredMovement.x = -self.speed*self.maxTransVelocity
 
             elif key == keyboard.Key.left:
-                self.desiredMovement.z = 7*self.speed*np.sign(self.desiredMovement.x)
+                if self.desiredMovement.x >= 0:
+                    self.desiredMovement.z = 7*self.speed*self.maxRotVelocity
+                else:
+                    self.desiredMovement.z = -7*self.speed*self.maxRotVelocity
 
             elif key == keyboard.Key.right:
-                self.desiredMovement.z = -7*self.speed*np.sign(self.desiredMovement.x)
+                if self.desiredMovement.x >= 0:
+                    self.desiredMovement.z = -7*self.speed*self.maxRotVelocity
+                else:
+                    self.desiredMovement.z = 7*self.speed*self.maxRotVelocity
 
             elif key == keyboard.Key.shift:
-                if self.speed <= self.maxTransVelocity:
+                if self.speed < 1:
                     self.speed += 0.01
                     speedChange = True
 
             elif key == keyboard.Key.ctrl:
-                if self.speed >= 0:
+                if self.speed > 0:
                     self.speed -= 0.01
                     speedChange = True
 
             elif key == keyboard.Key.space:
+                if not self.speed == 0:
                     self.speed = 0.0
                     speedChange = True
 
@@ -96,38 +98,33 @@ class Computer_Node(Node):
             self.desiredMovement.z = 0.0
 
         elif key == keyboard.Key.right:
-            self.desiredMovement.z  = 0.0
+            self.desiredMovement.z = 0.0
 
 
     def updateVelocity(self):
         action = Twist()
         
-        distanceToDesiredVelocity = self.desiredMovement - self.currentMovement
-        
-        if distanceToDesiredVelocity.x > 0 or distanceToDesiredVelocity.z > 0:
-            self.currentMovement.x += min(distanceToDesiredVelocity.x, self.acceleration)
-            self.currentMovement.z += min(distanceToDesiredVelocity.x, 7*self.acceleration)
-            
-            
-            action.linear.x = self.currentMovement.x
-            action.angular.z = self.currentMovement.z
-            self.pub_action.publish(action)
-            
-            print(f"Current velocity: {self.currentMovement.x} m/s, {self.currentMovement.z} rad/s")
-            print(f"Desired velocity: {self.desiredMovement.x} m/s, {self.desiredMovement.z} rad/s\n")
+        distanceToDesiredVelocity = Vector3()
+        distanceToDesiredVelocity.x = self.desiredMovement.x - self.currentMovement.x
+        distanceToDesiredVelocity.y = self.desiredMovement.y - self.currentMovement.y
+        distanceToDesiredVelocity.z = self.desiredMovement.z - self.currentMovement.z
 
-    def updateControllerInput(self):
-        if self.controller:
-            try:
-                events = get_gamepad()
-                for event in events:
-                    if event.code == "HX":
-                        self.desiredMovement.x = (event.state/32768)*self.maxTransVelocity
-                    elif event.code == "HY":
-                        self.desiredMovement.z = -(event.state/32768)*self.maxRotVelocity*np.sign(self.desiredMovement.x)
-            except UnpluggedError:
-                print("No gamepad plugged in. Gamepad support will be disabled from now on!")
-                self.controller = False
+        if abs(distanceToDesiredVelocity.x) >= self.acceleration:
+            self.currentMovement.x += self.acceleration*(2*(distanceToDesiredVelocity.x>0)-1)
+        else:
+            self.currentMovement.x += distanceToDesiredVelocity.x
+
+        if abs(distanceToDesiredVelocity.z) >= 14*self.acceleration:
+            self.currentMovement.z += 14*self.acceleration*(2*(distanceToDesiredVelocity.z>0)-1)
+        else:
+            self.currentMovement.z += distanceToDesiredVelocity.z
+
+        action.linear.x = self.currentMovement.x
+        action.angular.z = self.currentMovement.z
+        self.pub_action.publish(action)
+
+        print(f"Current velocity: {self.currentMovement.x} m/s, {self.currentMovement.z} rad/s")
+        print(f"Desired velocity: {self.desiredMovement.x} m/s, {self.desiredMovement.z} rad/s\n")
 
 def main(args=None):
     rclpy.init(args=args)
