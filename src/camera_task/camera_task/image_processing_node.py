19bcdf49923db372563f90d2d3f55e512940f9b6
Fabian Kievelitz <fabian.kievelitz@ruhr-uni-bochum.de>
2022-11-28 18:31:00 +0100

    added yolov5 implementation

--- src/camera_task/camera_task/image_processing_node.py
+++ src/camera_task/camera_task/image_processing_node.py
@@ -7,6 +7,13 @@ from sensor_msgs.msg import Image, CompressedImage
 
 import datetime
 
+import numpy as np
+
+from yolov5.utils.general import scale_boxes, non_max_suppression
+from yolov5.models.common import DetectMultiBackend
+from yolov5.utils.plots import Annotator, colors
+
+import torch
 
 class ImgProcessingNode(Node):
 
@@ -17,16 +24,41 @@ class ImgProcessingNode(Node):
         self.subscriber_img = self.create_subscription(Image, '/raw_image', self.callback, 10)
         # publisher for compressed img data
         self.publisher_ = self.create_publisher(CompressedImage, '/proc_img', 10)
+        
+        #Initializing the yolov5 model
+        self.targetWidth = 640
+        self.targetHeight = 640
+        self.classes = ['blue', 'orange', 'yellow']
+        self.model = DetectMultiBackend('best-int8_edgetpu.tflite')
+        self.model.warmup(imgsz=(1, self.targetWidth, self.targetHeight, 3))
 
     def callback(self, msg):
         print(f"received new image {datetime.datetime.now()}")
 
-        img = self.bridge.imgmsg_to_cv2(msg)
+        original_image = self.bridge.imgmsg_to_cv2(msg)
+        
+        #preparing image for yolov5 network
+        original_image = cv2.resize(original_image, (self.targetWidth, self.targetHeight))
+        prepared_image = torch.from_numpy(cv2.dnn.blobFromImage(original_image, 1/255, (self.targetWidth, self.targetHeight), swapRB=True, crop=False).astype(np.uint8)).to(self.model.device)
+
+        #running the yolov5 network on the image
+        pred = self.model(prepared_image)
+        pred = non_max_suppression(pred, 0.25, 0.45, [0, 1, 2], False, max_det=1000)
+        
+        
+        #annotating image with detected bounding boxes
+        annotator = Annotator(original_image)
+        for *xyxy, conf, cls in pred[0]:
+            if conf > 0.7:
+                annotator.box_label(xyxy, f'{self.classes[int(cls)]} {conf:.2f}')
+
+        result_image = annotator.result()
 
         # convert image to compressed image
-        compressed_image = self.bridge.cv2_to_compressed_imgmsg(img)
+        compressed_image = self.bridge.cv2_to_compressed_imgmsg(result_image)
         # publish compressed image
         self.publisher_.publish(compressed_image)
+        
 
 
 def main(args=None):
