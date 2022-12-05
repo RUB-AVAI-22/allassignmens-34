79d6f708df72437066a0ace069e54db8b50d15f6
kieveffv <fabian.kievelitz@rub.de>
2022-12-05 15:51:38 +0100

    adjusted msg file and got it to build

--- src/install/avai_messages/share/avai_messages/package.sh
+++ src/install/avai_messages/share/avai_messages/package.sh
@@ -79,6 +79,8 @@ _colcon_package_sh_source_script() {
 
 # source sh hooks
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/avai_messages/hook/cmake_prefix_path.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/avai_messages/hook/ld_library_path_lib.sh"
+_colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/avai_messages/hook/pythonpath.sh"
 _colcon_package_sh_source_script "$COLCON_CURRENT_PREFIX/share/avai_messages/local_setup.sh"
 
 unset _colcon_package_sh_source_script
