b72e7d740ba80970c21a9725762378200db2259b
kieveffv <fabian.kievelitz@ruhr-uni-bochum.de>
2023-01-11 18:00:42 +0100

    fixed message bugs

---
+++ src/install/avai_messages/include/avai_messages/msg/bounding_box_with_real_coordinates.h
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_c/resource/idl.h.em
+// with input from avai_messages:msg/BoundingBoxWithRealCoordinates.idl
+// generated code does not contain a copyright notice
+
+#ifndef AVAI_MESSAGES__MSG__BOUNDING_BOX_WITH_REAL_COORDINATES_H_
+#define AVAI_MESSAGES__MSG__BOUNDING_BOX_WITH_REAL_COORDINATES_H_
+
+#include "avai_messages/msg/detail/bounding_box_with_real_coordinates__struct.h"
+#include "avai_messages/msg/detail/bounding_box_with_real_coordinates__functions.h"
+#include "avai_messages/msg/detail/bounding_box_with_real_coordinates__type_support.h"
+
+#endif  // AVAI_MESSAGES__MSG__BOUNDING_BOX_WITH_REAL_COORDINATES_H_
