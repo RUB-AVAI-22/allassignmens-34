621a302a555e0396f07401f5651e309a7f20e7ee
ng-ineer <kogwah.ng@googlemail.com>
2023-02-16 00:09:23 +0100

    basic framework added

--- src/camera_task/test/test_map_display_node.py
+++ src/camera_task/test/test_map_display_node.py
@@ -1,3 +1,24 @@
 import unittest
 import rclpy
+
 from src.camera_task.camera_task.map_display_node import MapDisplayNode
+from unittest import TestCase
+
+class MapDisplayNodeTest(TestCase):
+    @classmethod
+    def setUp(cls) -> None:
+        rclpy.init()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.map_display_node = MapDisplayNode()
+
+    def tearDown(self):
+        self.map_display_node.destroy_node()
+
+
+if __name__ == '__main__':
+    unittest.main()
