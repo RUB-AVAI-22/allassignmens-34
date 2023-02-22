cf9034a9e3156dc40308e8a7632ffe52abcd51d4
kieveffv <fabian.kievelitz@ruhr-uni-bochum.de>
2023-02-22 16:21:38 +0100

    changed qos profile for lidar

--- src/camera_task/camera_task/sensor_fusion_node.py
+++ src/camera_task/camera_task/sensor_fusion_node.py
@@ -23,7 +23,7 @@ class SensorFusionNode(Node):
         super().__init__('sensor_fusion_node')
 
         self.boundingBox_subscriber = message_filters.Subscriber(self, BoundingBoxes, '/bboxes')
-        self.lidar_subscriber = message_filters.Subscriber(self, LaserScan, '/scan', qos_profile_sensor_data)
+        self.lidar_subscriber = message_filters.Subscriber(self, LaserScan, '/scan')
         self.odom_subscriber = message_filters.Subscriber(self, Odometry, '/odom')
         self.synchronizer = message_filters.ApproximateTimeSynchronizer([self.boundingBox_subscriber, self.lidar_subscriber, self.odom_subscriber], 100, 0.1)
         self.synchronizer.registerCallback(self.callback_synchronized)
