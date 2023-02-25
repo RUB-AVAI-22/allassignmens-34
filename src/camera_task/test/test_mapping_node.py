f70b3b237da2f6c49207ad43f81fcdaa3228f04d
ng-ineer <kogwah.ng@googlemail.com>
2023-02-25 13:59:39 +0100

    test message distance

--- src/camera_task/test/test_mapping_node.py
+++ src/camera_task/test/test_mapping_node.py
@@ -1,5 +1,6 @@
 import unittest
 import rclpy
+import datetime
 
 from src.camera_task.camera_task.mapping_node import MappingNode
 from unittest import TestCase
@@ -18,6 +19,10 @@ class MappingNodeTest(TestCase):
 
     def tearDown(self):
         self.mapping_node.destroy_node()
+
+    def test_message_distance(self):
+        assert self.mapping_node.message_distance is None
+        assert self.mapping_node.message_distance(self) <=0
 
 
 if __name__ == '__main__':
