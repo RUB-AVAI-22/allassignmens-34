225b69b7b776a58e4eccae81e900d7c677857b07
ng-ineer <kogwah.ng@googlemail.com>
2023-02-25 15:10:45 +0100

    test remove old message

--- src/camera_task/test/test_mapping_node.py
+++ src/camera_task/test/test_mapping_node.py
@@ -1,6 +1,6 @@
 import unittest
 import rclpy
-import datetime
+
 
 from src.camera_task.camera_task.mapping_node import MappingNode
 from unittest import TestCase
@@ -20,9 +20,13 @@ class MappingNodeTest(TestCase):
     def tearDown(self):
         self.mapping_node.destroy_node()
 
+    def test_remove_old_messages(self):
+        assert not self.mapping_node.remove_old_messages().empty() is None
+
     def test_message_distance(self):
         assert self.mapping_node.message_distance is None
         assert self.mapping_node.message_distance(self) <=0
+
 
 
 if __name__ == '__main__':
