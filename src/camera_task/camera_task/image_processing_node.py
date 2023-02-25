adefecde254ef7e822588583e9e1a979f0694297
Yusuf Tural <yusuf.tural@rub.de>
2023-02-25 13:29:41 +0100

    changed processed image header

--- src/camera_task/camera_task/image_processing_node.py
+++ src/camera_task/camera_task/image_processing_node.py
@@ -108,6 +108,7 @@ class ImageProcessingNode(Node):
             self.get_logger().info('no bounding boxes')
         # convert image to compressed image
         compressed_image = self.bridge.cv2_to_compressed_imgmsg(original_image)
+        compressed_image.header = msg.header
         # publish compressed image
         self.compressed_image_publisher.publish(compressed_image)
 
